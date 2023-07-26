435ed70f8b69cab74eb35a13754f1d52507e743d
Nikita <nickvoroninwork@gmail.com>
2023-07-26 14:23:47 +0800

    added button

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -2,6 +2,7 @@ const Chat = () => {
   return (
     <div className="text-xl font-bold  ml-3 ">
         ЭТОТ ЛОЛЬНЫЙ ТЕКСТ НАХОДИТСЯ ВНУТРИ DIV
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">My Tailwind Button</button>
       </div>
   )
 }
