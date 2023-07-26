ab83b93e646240fa055f53e86d62258e3e4d542d
Nikita <nickvoroninwork@gmail.com>
2023-07-26 15:38:42 +0800

    added button

--- src/components/CanvasContainer.tsx
+++ src/components/CanvasContainer.tsx
@@ -25,47 +25,73 @@ const CanvasContainer = () => {
   const socket = useSocket('http://localhost:4000', handleSocketMessage);
 
   useEffect(() => {
-    socket.emit('message', { name, color });
-  }, [color, name, socket]);
-
-  useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       let newPosition;
       switch (event.key) {
         case 'w':
-          newPosition = cubes.find(cube => cube.name === name)?.position;
-          newPosition && socket.emit('message', { name, color, position: [newPosition[0], newPosition[1], newPosition[2] + 1] });
+          newPosition = cubes.find((cube) => cube.name === name)?.position;
+          newPosition && setCubes((prevCubes) =>
+            prevCubes.map((cube) =>
+              cube.name === name ? { ...cube, position: [cube.position[0], cube.position[1], cube.position[2] + 1] } : cube
+            )
+          );
           break;
         case 'a':
-          newPosition = cubes.find(cube => cube.name === name)?.position;
-          newPosition && socket.emit('message', { name, color, position: [newPosition[0] - 1, newPosition[1], newPosition[2]] });
+          newPosition = cubes.find((cube) => cube.name === name)?.position;
+          newPosition && setCubes((prevCubes) =>
+            prevCubes.map((cube) =>
+              cube.name === name ? { ...cube, position: [cube.position[0] - 1, cube.position[1], cube.position[2]] } : cube
+            )
+          );
           break;
         case 's':
-          newPosition = cubes.find(cube => cube.name === name)?.position;
-          newPosition && socket.emit('message', { name, color, position: [newPosition[0], newPosition[1], newPosition[2] - 1] });
+          newPosition = cubes.find((cube) => cube.name === name)?.position;
+          newPosition && setCubes((prevCubes) =>
+            prevCubes.map((cube) =>
+              cube.name === name ? { ...cube, position: [cube.position[0], cube.position[1], cube.position[2] - 1] } : cube
+            )
+          );
           break;
         case 'd':
-          newPosition = cubes.find(cube => cube.name === name)?.position;
-          newPosition && socket.emit('message', { name, color, position: [newPosition[0] + 1, newPosition[1], newPosition[2]] });
+          newPosition = cubes.find((cube) => cube.name === name)?.position;
+          newPosition && setCubes((prevCubes) =>
+            prevCubes.map((cube) =>
+              cube.name === name ? { ...cube, position: [cube.position[0] + 1, cube.position[1], cube.position[2]] } : cube
+            )
+          );
           break;
       }
+      // Emit the message to the server here if needed
     };
 
     window.addEventListener('keydown', handleKeyDown);
+
     
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
+      console.log(name, color, cubes)
     };
   }, [name, color, cubes, socket]);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); 
+
+    if (name && color) {
+      const newPosition: [number, number, number] = [0, 0, 0];
+      setCubes([...cubes, { name, color, position: newPosition }]);
+    }
+  };
+
 
   return (
     <Canvas className='bg-green-500 h-screen'>
       <Html>
-        <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
-        <input type="color" value={color} onChange={(e) => setColor(e.target.value)} />
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+          <input type="color" value={color} onChange={(e) => setColor(e.target.value)} />
+          <button type="submit">Add Cube</button>
+        </form>
       </Html>
-
-      
         <Physics>
           {cubes.map((cube) => (
             <Cube key={cube.name} name={cube.name} color={cube.color} position={cube.position} />
--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
 import { useBox } from '@react-three/cannon';
-import { Text } from "@react-three/drei";
+import { Text } from '@react-three/drei';
+import { mesh, useBox } from '@react-three/fiber'; // Import mesh and useBox from @react-three/fiber
+import { BoxBufferGeometry, MeshStandardMaterial } from 'three'; // Import BoxBufferGeometry and MeshStandardMaterial from three.js
+
 interface CubeProps {
   color: string;
   position: [number, number, number];
@@ -9,17 +11,16 @@ interface CubeProps {
 
 export const Cube: React.FC<CubeProps> = (props) => {
   const [ref] = useBox(() => ({ mass: 1, position: props.position }));
-const meshRef = ref as React.MutableRefObject<THREE.Mesh>;
 
   return (
     <>
-      <mesh ref={meshRef} >
-        <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-        <meshStandardMaterial attach="material" color={props.color} />
+      <mesh ref={ref}>
+        <boxBufferGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={props.color} />
       </mesh>
       <Text position={[props.position[0], props.position[1] + 1.5, props.position[2]]} fontSize={0.5}>
-      {props.name}
-    </Text>
+        {props.name}
+      </Text>
     </>
   );
 };
