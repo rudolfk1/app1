a43c8d5b5c852c81df792ba07ca75d283a810aad
rudolfk1 <kurzanovrudolf@gmail.com>
2023-07-26 23:04:06 -0400

    okok

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import * as React from 'react';
+import* as ReactDOM from 'react-dom/client'
+
 import './index.css'
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
